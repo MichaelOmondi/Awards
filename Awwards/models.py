7dfa62e657d8bbd13d9755d70f3bf7d160f874d7
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:18:41 +0300

    create project model

--- Awwards/models.py
+++ Awwards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+#Project models
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    link = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    image = models.ImageField(upload_to='project_images', blank=True)
+    user_project_id = models.IntegerField(default=0)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    creativity = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+
+

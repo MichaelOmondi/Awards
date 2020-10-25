1dcaf7efbc0dee941d9e43564fd66cd6294d4829
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:50:19 +0300

    create the signup views

--- Awwards/views.py
+++ Awwards/views.py
@@ -21,3 +21,47 @@ def index(request):
     except Project.DoesNotExist:
         posts = None
     return render(request, 'main/index.html', {'posts': posts, 'form': form})
+
+# Signup 
+def signup(request):
+    global register_form
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+        register_form = {
+            'form': form,
+        }
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    return render(request, 'profile/profile.html')
+
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        uform = UpdateUserForm(instance=request.user)
+        pform = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': uform,
+        'prof_form': pform,
+    }
+    return render(request, 'main/edit.html', params)

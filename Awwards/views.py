6a7d4d4f9723cfe15858e54546c699d67319c09b
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:59:34 +0300

    create the rate project_view views

--- Awwards/views.py
+++ Awwards/views.py
@@ -100,5 +100,54 @@ def rate(request):
     ratings = Rate.objects.all()
     rate_params = {
         'ratings': ratings
-    }    
+    }   
+
+    return render('main/view_project.html', rate_params)
+
+# Profile views
+def profile_view(request):
+    profile = Profile.get_profile_data()
+    profile_data = {
+        'profile': profile
+    }
+
+    return render('profile/profile.html', profile_data)
+
+# Project_view views
+def project_view(request, post):
+    post = Project.objects.get(title=post)
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rate.objects.filter(post=post)
+
+            design_ratings = [i.design for i in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [i.usability for i in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [i.content for i in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+
+    else:
+        form = RatingsForm()
+    data = {
+        'post': post,
+        'rating_form': form
+
+    }
+    return render(request, 'main/view_project.html', data)
+         
 

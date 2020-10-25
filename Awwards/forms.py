83ec892f243c2839aecd949e8285ad2c49070acc
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:06:12 +0300

    create the sighnup form

---
+++ Awwards/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
+
+from .models import Profile, Project, Rate
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+
+
+        

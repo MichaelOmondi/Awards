8f1542d20fc22829abf58cb79baf9a965a5d0afa
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:20:47 +0300

    create profile model

--- Awwards/models.py
+++ Awwards/models.py
@@ -16,5 +16,73 @@ class Project(models.Model):
     content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     vote_submissions = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def fetch_all_images(cls):
+        all_images = Project.objects.all()
+        return all_images
+
+    @classmethod
+    def get_single_project(cls, project):
+        project = cls.objects.get(id=project)
+        return project
+
+    @classmethod
+    def search_project_by_title(cls, search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
+
+    class Meta:
+        db_table = 'projects'
+        ordering = ['-id']
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=80, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.comment
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    class Meta:
+        db_table = 'comments'
+        ordering = ["-id"]
+
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    prof_user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    bio = models.TextField(default="")
+    contact_info = models.CharField(max_length=200, blank=True)
+    profile_Id = models.IntegerField(default=0)
+    all_projects = models.ForeignKey('Project', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self, bio):
+        self.bio = bio
+        self.save()
+
+    @classmethod
+    def get_profile_data(cls):
+        return Profile.objects.all()
+
+    class Meta:
+        db_table = 'profiles'
+
 
 

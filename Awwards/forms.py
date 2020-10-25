0ef5952202f8c427348a61844c6733ea1c844b7d
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:12:32 +0300

    create the update user form

--- Awwards/forms.py
+++ Awwards/forms.py
@@ -28,3 +28,9 @@ class RatingsForm(forms.ModelForm):
     class Meta:
         model = Rate
         fields = ['design', 'usability', 'content', 'creativity']
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'contact_info']

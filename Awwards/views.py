afc0dbe9d832dae198f6a13b228e5830eb42303d
MichaelOmondi <omondimike11@gmail.com>
2020-10-25 15:48:23 +0300

    create the index views

--- Awwards/views.py
+++ Awwards/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from .forms import *
+
+# Creating views
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Project.objects.all()
+        print(posts)
+    except Project.DoesNotExist:
+        posts = None
+    return render(request, 'main/index.html', {'posts': posts, 'form': form})

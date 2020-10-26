a24b25f32e89d50f0fafc447d9dd304359ee14b8
MichaelOmondi <omondimike11@gmail.com>
2020-10-26 20:33:15 +0300

    added the profile

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -1,18 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path, path, include
-
+from . import views
 from .views import home, project_view, signup, profile, edit_profile, upload, index
 
 # Creating the urls
 
 urlpatterns = [
     path('', home, name='home'),
-    path('project/<post>', project_view, name='project'),
-    path('signup/', signup, name='signup'),
-    path('upload/', upload, name='upload'),
-    path('profile/<username>/', profile, name='profile'),
-    path('profile/<username>/settings', edit_profile, name='edit'),
+    path('project/<post>',views.project_view, name='project'),
+    path('signup/', views.signup, name='signup'),
+    path('upload/', views.upload, name='upload'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
     path('account/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
